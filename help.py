19684027d2c9ce0c64c1a834c3e9ec85cf4cfbfb
Nandagopal Menon <71086360+nanduuuseee@users.noreply.github.com>
2021-07-30 22:30:21 +0530

    Added tier commands

--- help.py
+++ help.py
@@ -23,101 +23,108 @@ class Command:
         self.aliases = aliases
         self.name = name
 
-
-math = CommandGroup('**🤓 Math**',
-                    '`$help math`',
-                    'math')
-add = Command(math,
-              'Add',
-              'Adds given numbers and returns the sum.',
-              '`$add <[a b c ...]>`',
-              name='add',
-              output='`a + b + c + ... = (sum)`')
-subtract = Command(math,
-                   'Subtract',
-                   'Subtracts two given numbers and returns the difference.',
-                   '`$subtract <x> <y>`',
-                   output='`x - y = (difference)`', 
-                   name='subtract')
-mult = Command(math,
-               'Multiply',
-               'Mulitplies given numbers and returns the product.',
-               '`$mult <[a b c ...]>`',
-               output='`a * b * c * ... = (product)`', 
-               name='mult')
-div = Command(math,
-              'Divide',
-              'Divides two given numbers and returns the quotient.',
-              '`$div <x> <y>`',
-              output='`x / y = (quotient)`', 
-                name='div')
-mean = Command(math,
-               'Mean',
-               'Gives the arithmetic mean or average of a list of given numbers.',
-               '`$mean <[a b c ...]>`',
-               output='`The arithmetical mean of [a b c ...] is (mean)`',
-               name='mean')
-median = Command(math,
-                 'Median',
-                 'Gives the median of a list of given numbers.',
-                 '`$median <[a b c ...]>`',
-                 output='`The median of [a b c ...] is (median)`',
-                 name='median')
-mode = Command(math,
-               'Mode',
-               'Gives the arithemetic mode of given list of numbers.',
-               '`$mode <[a b c ...]>`',
-               output='`The mode of [a b c ...] is (mode) occuring at a total of (count) times.`', name='mode')
-
-listener = CommandGroup('**👂 Listener**',
-                        '`$help listener`',
-                        'listener')
-list = Command(listener, 'List',
-               'Shows a list of all the keys and replies on this server.', '`$listener list`', name='list')
-ladd = Command(listener, 'Add',
-               'Add a reply to an existing key or create a new key.', '`$listener add |key|`', name='add')
-toggle = Command(listener, 'Toggle Key',
-                 'Toggles a key on or off for the server.', '`$listener toggle |key|`', name='toggle')
-enable = Command(listener, 'Enable Listener',
-                 'Enables all listeneres for this server.', '`$listener enable`', name='enable')
-disable = Command(listener, 'Disable listener',
-                  'Disables all listeners for this server.', '`$listener enable`', name='disable')
-remove = Command(listener, 'Remove Reply',
-                 'Removes specific replies from keys.', '`$listener remove |key|`', name='remove')
-
-fun = CommandGroup('**🎮 Fun**',
-                   '`$help fun`',
-                   'fun')
-xo = Command(fun, 'TicTacToe', 'Play TicTacToe with another player!',
-             '`$xo <@player 2>`', aliases=['`ttt`', '`tictactoe`'], name='xo')
-rps = Command(fun, 'Rock Paper Scissors',
-              'I play a game of rock paper scissors with you!', '`$rps`', name='rps')
-
-misc = CommandGroup('**✨ Misc**',
-                    '`$help misc`',
-                    'misc')
-coolbot = Command(misc, 'Cool Bot 😎',
-                  'Find out for yourself. ;)', '`$coolbot`', name='coolbot')
-
-groups = [math, listener, fun, misc]
-group_names = [group.name for group in groups]
-
-math.commands = [add, subtract, mult, div, mean, median, mode]
-listener.commands = [enable, disable, list, ladd, toggle, remove]
-fun.commands = [xo, rps]
-misc.commands = [coolbot]
-
-all_command_names = []
-for g in groups:
-    for c in g.commands:
-        all_command_names.append(c.name)
-
 class Cog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.group(name='help', invoke_without_command=True)
     async def help(self, ctx, group=None, subcommand=None):
+        math = CommandGroup('**🤓 Math**',
+                    f'`{ctx.prefix}help math`',
+                    'math')
+        add = Command(math,
+                    'Add',
+                    'Adds given numbers and returns the sum.',
+                    f'`{ctx.prefix}add <[a b c ...]>`',
+                    name='add',
+                    output='`a + b + c + ... = (sum)`')
+        subtract = Command(math,
+                        'Subtract',
+                        'Subtracts two given numbers and returns the difference.',
+                        f'`{ctx.prefix}subtract <x> <y>`',
+                        output='`x - y = (difference)`', 
+                        name='subtract')
+        mult = Command(math,
+                    'Multiply',
+                    'Mulitplies given numbers and returns the product.',
+                    f'`{ctx.prefix}mult <[a b c ...]>`',
+                    output='`a * b * c * ... = (product)`', 
+                    name='mult')
+        div = Command(math,
+                    'Divide',
+                    'Divides two given numbers and returns the quotient.',
+                    f'`{ctx.prefix}div <x> <y>`',
+                    output='`x / y = (quotient)`', 
+                        name='div')
+        mean = Command(math,
+                    'Mean',
+                    'Gives the arithmetic mean or average of a list of given numbers.',
+                    f'`{ctx.prefix}mean <[a b c ...]>`',
+                    output='`The arithmetical mean of [a b c ...] is (mean)`',
+                    name='mean')
+        median = Command(math,
+                        'Median',
+                        'Gives the median of a list of given numbers.',
+                        f'`{ctx.prefix}median <[a b c ...]>`',
+                        output='`The median of [a b c ...] is (median)`',
+                        name='median')
+        mode = Command(math,
+                    'Mode',
+                    'Gives the arithemetic mode of given list of numbers.',
+                    f'`{ctx.prefix}mode <[a b c ...]>`',
+                    output='`The mode of [a b c ...] is (mode) occuring at a total of (count) times.`', name='mode')
+
+        listener = CommandGroup('**👂 Listener**',
+                                f'`{ctx.prefix}help listener`',
+                                'listener')
+        list = Command(listener, 'List',
+                    'Shows a list of all the keys and replies on this server.', f'`{ctx.prefix}listener list`', name='list')
+        ladd = Command(listener, 'Add',
+                    'Add a reply to an existing key or create a new key.', f'`{ctx.prefix}listener add |key|`', name='add')
+        toggle = Command(listener, 'Toggle Key',
+                        'Toggles a key on or off for the server.', f'`{ctx.prefix}listener toggle |key|`', name='toggle')
+        enable = Command(listener, 'Enable Listener',
+                        'Enables all listeneres for this server.', f'`{ctx.prefix}listener enable`', name='enable')
+        disable = Command(listener, 'Disable listener',
+                        'Disables all listeners for this server.', f'`{ctx.prefix}listener enable`', name='disable')
+        remove = Command(listener, 'Remove Reply',
+                        'Removes specific replies from keys.', f'`{ctx.prefix}listener remove |key|`', name='remove')
+
+        fun = CommandGroup('**🎮 Fun**',
+                        f'`{ctx.prefix}help fun`',
+                        'fun')
+        xo = Command(fun, 'TicTacToe', 'Play TicTacToe with another player!',
+                    f'`{ctx.prefix}xo <@player 2>`', aliases=['`ttt`', '`tictactoe`'], name='xo')
+        rps = Command(fun, 'Rock Paper Scissors',
+                    'I play a game of rock paper scissors with you!', f'`{ctx.prefix}rps`', name='rps')
+
+        misc = CommandGroup('**✨ Misc**',
+                            f'`{ctx.prefix}help misc`',
+                            'misc')
+        coolbot = Command(misc, 'Cool Bot 😎',
+                        'Find out for yourself. ;)', f'`{ctx.prefix}coolbot`', name='coolbot')
+
+        tier = CommandGroup('**🎖 Messaging Tiers**', f'{ctx.prefix}help tier', 'tier')
+        deduct = Command(tier, 'Deduct Point', 'Deducts messaging points from a user. (Admins Only)', f'`{ctx.prefix}tier deduct |mention member|`', 'deduct')
+        score = Command(tier, 'Messaging Score', 'Shows score and rank of a person or yourself in this server.', f'`{ctx.prefix}tier score |mention member|`', 'score')
+        exclude = Command(tier, 'Exclude Channel', 'Excludes said channel from adding up your scores. Useful to nerf spam channels etc.', f'`{ctx.prefix}tier exclude |#channel|`', 'exclude')
+        excluded = Command(tier, 'Excluded Channels List', 'Shows list of all the channels excluded from adding scores.', f'`{ctx.prefix}tier excluded`', 'excluded', aliases=['`list`', '`leaderboard`'])   
+        include = Command(tier, 'Iclude Channel', 'Includes said channel to add scores.', f'`{ctx.prefix}tier include |#channel|`', 'include')
+        leaderboard = Command(tier, 'Leaderboard', 'Shows the leaderboard of messaging scores for this server', f'`{ctx.prefix}leaderboard`', 'leaderboard', aliases=['`lb`', '`leaderboard`'])
+
+        groups = [math, listener, tier, fun, misc]
+        group_names = [group.name for group in groups]
+
+        math.commands = [add, subtract, mult, div, mean, median, mode]
+        listener.commands = [enable, disable, list, ladd, toggle, remove]
+        tier.commands = [leaderboard, score, include, exclude, excluded, deduct]
+        fun.commands = [xo, rps]
+        misc.commands = [coolbot]
+
+        all_command_names = []
+        for g in groups:
+            for c in g.commands:
+                all_command_names.append(c.name)
         if group == None:
             help = discord.Embed(title='Chitti Robo commands list',
                                  description='Help command for this bot.', color=PURPLE)
@@ -135,7 +142,7 @@ class Cog(commands.Cog):
             for c in group.commands:
                 help.add_field(name=c.command, value=c.usage, inline=True)
             help.set_footer(
-                text=f'Use `$help {group.name} <command>` for more info on each command.\nSyntax: <required> [infinite list of numbers...] |optional|')
+                text=f'Use `{ctx.prefix}help {group.name} <command>` for more info on each command.\nSyntax: <required> [infinite list of numbers...] |optional|')
             await ctx.reply(embed=help, mention_author=False)
 
         elif group in group_names and subcommand in all_command_names:
@@ -161,10 +168,6 @@ class Cog(commands.Cog):
 
             help.set_footer(text='Syntax: <required> [infinite list of numbers...] |optional|')
             await ctx.reply(embed=help)
-
-    @commands.command()
-    async def testing(self, ctx):
-        await ctx.send('Testing actually worked.')
 
 def setup(bot):
     bot.add_cog(Cog(bot))
