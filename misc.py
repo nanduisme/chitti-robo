2f4def47dd9c4021237b2886e544e769a184617c
Nandagopal Menon <71086360+nandu-not-bot@users.noreply.github.com>
2021-09-06 21:44:39 +0530

    feedback

--- misc.py
+++ misc.py
@@ -9,6 +9,25 @@ class MiscCommands(commands.Cog):
     async def coolbot(self, ctx):
         await ctx.send("This bot is cool. :)")
 
+    @commands.command()
+    async def feedback(self, ctx: commands.context.Context, *feedback):
+
+        """Get feedback from the users"""
+
+        if len(feedback) == 0:
+            await ctx.send(
+                "❌ Oops! I think you forgot to give your feedback!", delete_after=5
+            )
+            return
+
+        dev = self.bot.get_user(591078175778537512)
+        feedback = "".join(f"{word} " for word in feedback)
+
+        await dev.send(f"{ctx.author.mention} says: " + feedback)
+        await ctx.reply(
+            "Team Chitti thanks you for your valuable feedback! 😄", delete_after=30
+        )
+
 
 def setup(bot):
     bot.add_cog(MiscCommands(bot))
